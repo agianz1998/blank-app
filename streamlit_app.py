945bb0163931d6d5a0513cdf055843e7b04f8b1f
agianz1998 <71777519+agianz1998@users.noreply.github.com>
2025-02-04 12:36:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ for index, row in st.session_state.df.iterrows():
     cols = st.columns([3, 2, 3, 2])
     cols[0].write(row['Name'])
     cols[1].write(str(row['Age']))
-#    cols[2].write(row['status'])
+    cols[2].write(row['status'])
     if cols[3].button('Edit', key=f"edit_{index}"):
         st.session_state.edit_index = index
 
