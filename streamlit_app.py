d9cdd839cfcb268e2193bcd4b03ca78119410814
agianz1998 <71777519+agianz1998@users.noreply.github.com>
2025-02-04 10:13:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 Checkbox Toggle Test")
+#st.write(
+#    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+#)
+
+if 'df' not in st.session_state:
+    st.session_state.df = pd.DataFrame({'id':[1,2,3], 'active':[False, False, False]})
+column_config = {'active':st.column_config.CheckboxColumn('Active Status', disables=st.session_state.df['active'].tolist())}
+
+st.write("**Before Edit:**", st.session_state.df['active'].tolist())
+edited_df = st.data_editor(st.session_state.df, column_config = column_config, key = "unique_editor_key", use_container_width = True)
+
+if not edited_df.equals(st.session_state.df):
+    st.session_state.df = edited_df
+    st.rerun()
+
+st.write("**After Edit:**", st.session_state.df['active'].tolist())
